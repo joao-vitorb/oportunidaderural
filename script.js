9b103f73bb2dfbb8d3c6836dbba3240826d23e0d
joao-vitorb <joaovborges2005@hotmail.com>
2024-09-02 13:30:38 -0300

    updated whatsapp url

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Código para animações e interações das opções
     const options = document.querySelectorAll('.option');
     options.forEach((option, index) => {
         option.style.animationDelay = `${index * 0.1}s`;
     });
 
-    // Código do carrossel de banners
     const carousel = document.querySelector('.carousel');
     const slides = document.querySelectorAll('.carousel-slide');
     let currentSlide = 0;
     const totalSlides = slides.length;
 
-    // Função para mostrar o slide atual e esconder os outros
     function updateCarousel() {
         slides.forEach((slide, index) => {
             if (index === currentSlide) {
@@ -34,32 +31,29 @@ document.addEventListener("DOMContentLoaded", function () {
 
     const slideInterval = setInterval(nextSlide, 5000);
 
-    // Controles de navegação do carrossel
     document.querySelector('.next-slide').addEventListener('click', () => {
-        clearInterval(slideInterval); // Parar o autoplay ao clicar
+        clearInterval(slideInterval);
         nextSlide();
     });
 
     document.querySelector('.prev-slide').addEventListener('click', () => {
-        clearInterval(slideInterval); // Parar o autoplay ao clicar
+        clearInterval(slideInterval);
         prevSlide();
     });
 
-    // Inicia o carrossel mostrando o primeiro slide
     updateCarousel();
 
-    // Código do menu
     const menuIcon = document.getElementById('menuIcon');
     const sidebar = document.getElementById('sidebar');
     const closeIcon = document.getElementById('closeIcon');
 
     menuIcon.addEventListener('click', () => {
-        sidebar.style.left = '0'; // Abre o menu
-        menuIcon.style.display = 'none'; // Oculta o ícone de menu
+        sidebar.style.left = '0'; 
+        menuIcon.style.display = 'none'; 
     });
 
     closeIcon.addEventListener('click', () => {
-        sidebar.style.left = '-250px'; // Fecha o menu
-        menuIcon.style.display = 'block'; // Mostra o ícone de menu novamente
+        sidebar.style.left = '-250px'; 
+        menuIcon.style.display = 'block'; 
     });
 });
